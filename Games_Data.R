9af4dc7c58a19487a4ede92ad68b5d28dd9a64f1
Preston Blake <124848277+PrestBlake25@users.noreply.github.com>
2025-09-25 22:48:30 -0400

    Add visualizations for Game data analysis
    # Creates visualizations for game data including histograms and bar charts.

--- Games_Data.R
+++ Games_Data.R
@@ -1,3 +1,14 @@
 library(readr)
 Game <- read_csv("Video Games Sales.csv")
 View(Game)
+hist(Game$Year, xlab = "Release Years", main="Game Releases", col = "blue", border = "yellow")
+#Creates a histogram of the years the games released
+
+barplot(table(Game$Genre), xlab = "Genres", ylab = "Frequency", main = "Genres of Games", col = "blue", border = "yellow")
+#Creates a bar chart with the Games' genre
+
+barplot(table(Game$Platform), xlab = "Genres", ylab = "Frequency", main = "Platforms for Gaming", col = "blue", border = "yellow")
+#Creates a barchart listing the Games' different platforms
+
+hist(Game$Global, xlab = "Game Sales Sold (in millions)", main = " Global Video Game Sales", col = "blue" , border = "yellow")
+# Makes a histogram demonstrating the global sales  
